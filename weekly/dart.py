f925973725952f18fa75c3057b25d38c72977153
nill <wwaosldk@gmail.com>
2021-09-26 14:38:53 +0900

    dart comment add

--- weekly/dart.py
+++ weekly/dart.py
@@ -5,6 +5,9 @@ import re
 def solution(dartResult):
     answer = 0
     resultList = []
+
+    # re.findall(pattern, string)은 string에서 pattern에 해당하는 내용들을 찾아서 리스트로 리턴
+    # r'\d+'는 1회 이상 반복되는 숫자들에 대한 패턴을 의미
     scores = re.findall(r'\d+', dartResult)
 
     dartResult = dartResult.split(scores[0],1)[1]
@@ -17,12 +20,15 @@ def solution(dartResult):
     for result in resultList:
         powTemp = 0
 
+        # 문자열 S, D, T를 제곱으로 계산하기 위해 임시 powTemp 변수에 저장
         if result[0] == "S":
             powTemp = 1
         if result[0] == "D":
             powTemp = 2
         if result[0] == "T":
             powTemp = 3
+        
+        # 옵션값이 있는지 확인
         if len(result) == 2:
             if result[1] == "*":
                 if len(totalScoreList) == 0:
@@ -40,5 +46,6 @@ def solution(dartResult):
 
     return answer
 
+# "점수|보너스|[옵션]"으로 이루어진 문자열 3세트
 dartResult = "1D2S#10S"
 print(solution(dartResult))
