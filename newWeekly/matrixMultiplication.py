402015b47dfe41f2ca4e2354970cc9a6e6b251ac
nill <wwaosldk@gmail.com>
2022-02-13 20:09:28 +0900

    study coding test practice weekly

---
+++ newWeekly/matrixMultiplication.py
@@ -0,0 +1,28 @@
+import numpy as np
+import array
+
+def solution(arr1, arr2):
+  ndarrayAnswer = np.matrix(arr1) * np.matrix(arr2)
+  return  ndarrayAnswer.tolist()
+
+def solution2(arr1, arr2):
+  answer = [[]]
+  print(arr1.size)
+  return answer
+
+ex1_arr1 = [[1, 4], [3, 2], [4, 1]]
+ex1_arr2 = [[3, 3], [3, 3]]
+
+ex2_arr1 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
+ex2_arr2 = [[5, 4, 3], [2, 4, 1], [3, 1, 1]]
+
+# 	[[15, 15], [15, 15], [15, 15]]
+print(solution(ex1_arr1, ex1_arr2))
+# [[22, 22, 11], [36, 28, 18], [29, 20, 14]]
+print(solution(ex2_arr1, ex2_arr2))
+
+
+
+
+# numpy 시간 복잡도
+# numpy 없이
