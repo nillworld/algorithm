cb034e6a8fccdea499f8a91f71b8a81f367b2f8f
nill <wwaosldk@gmail.com>
2022-02-20 14:22:39 +0900

    matrixt

--- newWeekly/matrixMultiplication.py
+++ newWeekly/matrixMultiplication.py
@@ -7,7 +7,8 @@ def solution(arr1, arr2):
 
 def solution2(arr1, arr2):
   answer = [[]]
-  print(arr1.size)
+  print(len(arr1))
+  print(len(arr1[0]))
   return answer
 
 ex1_arr1 = [[1, 4], [3, 2], [4, 1]]
@@ -17,7 +18,7 @@ ex2_arr1 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
 ex2_arr2 = [[5, 4, 3], [2, 4, 1], [3, 1, 1]]
 
 # 	[[15, 15], [15, 15], [15, 15]]
-print(solution(ex1_arr1, ex1_arr2))
+print(solution2(ex1_arr1, ex1_arr2))
 # [[22, 22, 11], [36, 28, 18], [29, 20, 14]]
 print(solution(ex2_arr1, ex2_arr2))
 
@@ -34,3 +35,4 @@ print(solution(ex2_arr1, ex2_arr2))
 
 # 형렬 곱셈의 관점
 # https://bskyvision.com/500
+# https://angeloyeo.github.io/2020/09/08/matrix_multiplication.html
