e01710906c4fb62fbbdbdc40622c32a35e2b5675
nill <wwaosldk@gmail.com>
2022-02-27 12:18:30 +0900

    check

--- newWeekly/targetNum.py
+++ newWeekly/targetNum.py
@@ -1,22 +1,27 @@
 # https://programmers.co.kr/learn/courses/30/lessons/43165
 from itertools import *
 def solution(numbers, target):
+    cntCheck = 0
     answer = 0
     # 부호 체크 0 == '-' / 1 == '+'
     checkSign = [0,1]
     # 부호 중복순열 리스트
     singProductList = list(product(checkSign, repeat=len(numbers)))
+    print(singProductList)
+    print(len(singProductList))
+    
     for singProduct in singProductList:
         # 임시로 계산된 값
         temp = 0
         for i in range(len(singProduct)):
+            cntCheck += 1
             if singProduct[i] == 0:
                 temp -= numbers[i]
             if singProduct[i] == 1:
                 temp += numbers[i]
         if temp == target:
             answer += 1
-    
+    print("cntCheck",cntCheck)
     return answer
 
 # answer = 5
