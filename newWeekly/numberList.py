c1d6205948897b2426a25102c50a512040b4484f
nill <wwaosldk@gmail.com>
2022-03-15 23:03:38 +0900

    numberlist

---
+++ newWeekly/numberList.py
@@ -0,0 +1,10 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42577
+
+def solution(phone_book):
+    answer = True
+    return answer
+  
+  
+phone_book_1 = ["119", "97674223", "1195524421"]
+phone_book_2 = ["123","456","789"]
+phone_book_3 = ["12","123","1235","567","88"]
