9c150b1b7fd7c7c4b7c5af3961664c6ca9323292
nill <wwaosldk@gmail.com>
2022-04-06 23:49:20 +0900

    get ground set

---
+++ js/getGround.js
@@ -0,0 +1,9 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12913
+function solution(land) {
+  var answer = 0;
+
+  // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+  console.log("Hello Javascript");
+
+  return answer;
+}
