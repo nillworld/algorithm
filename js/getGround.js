bddeb65e082b31fbbe81b76fd0772981d548fcae
nill <wwaosldk@gmail.com>
2022-04-07 23:09:47 +0900

    comment for solve

--- js/getGround.js
+++ js/getGround.js
@@ -6,10 +6,14 @@ function solution(land) {
     let land_line = land[i];
     let temp_top = 0;
     let temp_top_i = 0;
-    for(let j = 0; j < 4; j++){
-      if(temp_top<= tem)
+    for (let j = 0; j < 4; j++) {
+      if (temp_top <= tem) {
+      }
     }
   }
+
+  // land_line에서 가장 큰수랑 두번째로 큰수 차이가 나는 라인을 먼저 잡고.
+  // 그 위, 아래 라인의 해당 인덱스의 값을 0으로 초기화
 
   return answer;
 }
@@ -20,4 +24,5 @@ let land = [
   [5, 6, 8, 8],
   [4, 3, 2, 16],
 ];
+
 console.log(solution(land));
