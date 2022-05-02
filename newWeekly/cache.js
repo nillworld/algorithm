78e374be90c5aacee3dc0b175b76d582b86bb14b
nillworld <wwaosldk@gmail.com>
2022-05-03 08:36:40 +0900

    cache done

--- newWeekly/cache.js
+++ newWeekly/cache.js
@@ -6,18 +6,27 @@ function solution(cacheSize, cities) {
   let lowercaseCities = cities.map((city) => city.toLowerCase());
 
   for (i in lowercaseCities) {
-    if (cache.includes(lowercaseCities[i])) {
+    let cacheIndexCheck = cache.indexOf(lowercaseCities[i]);
+    if (cacheIndexCheck >= 0) {
       answer += 1;
+
+      // *두번째 실패요소... cache 중복 관리
+      // 비교할 요소가 캐시 마지막 인덱스에 있다면 넘어가고 없으면 기존에 있던걸 자르고 push
+      if (cache.indexOf(lowercaseCities[i]) !== cache.length - 1) {
+        cache.splice(cacheIndexCheck, 1);
+        cache.push(lowercaseCities[i]);
+      }
     } else {
       answer += 5;
-    }
-    if (cache.length < cacheSize) {
-      cache.push(lowercaseCities[i]);
-      console.log(cache);
-      // 1번 실패... cache가 0일 때
-    } else if (cacheSize > 1) {
-      cache.splice(0, 1);
-      cache.push(lowercaseCities[i]);
+      if (cache.length < cacheSize) {
+        cache.push(lowercaseCities[i]);
+        console.log(cache);
+
+        // *첫번째 실패요소... cache가 0일 때
+      } else if (cacheSize > 1) {
+        cache.splice(0, 1);
+        cache.push(lowercaseCities[i]);
+      }
     }
   }
   return answer;
@@ -32,8 +41,34 @@ const cacheSize6 = 0;
 const cacheSize7 = 0;
 const cities1 = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]; //50
 const cities2 = ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]; //21
-const cities3 = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]; //60
-const cities4 = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]; //52
+const cities3 = [
+  "Jeju",
+  "Pangyo",
+  "Seoul",
+  "NewYork",
+  "LA",
+  "SanFrancisco",
+  "Seoul",
+  "Rome",
+  "Paris",
+  "Jeju",
+  "NewYork",
+  "Rome",
+]; //60
+const cities4 = [
+  "Jeju",
+  "Pangyo",
+  "Seoul",
+  "NewYork",
+  "LA",
+  "SanFrancisco",
+  "Seoul",
+  "Rome",
+  "Paris",
+  "Jeju",
+  "NewYork",
+  "Rome",
+]; //52
 const cities5 = ["Jeju", "Pangyo", "NewYork", "newyork"]; //16
 const cities6 = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]; //25
 const cities7 = ["Jeju", "Jeju"]; //10
