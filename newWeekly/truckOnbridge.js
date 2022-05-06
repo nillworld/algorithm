456695783710764fd49065db7803964945a803ee
nillworld <wwaosldk@gmail.com>
2022-05-06 15:25:08 +0900

    truck ing....

--- newWeekly/truckOnbridge.js
+++ newWeekly/truckOnbridge.js
@@ -6,13 +6,20 @@ function solution(bridge_length, weight, truck_weights) {
   tempTrucks.length = bridge_length;
   tempTrucks.fill(0);
   let tempWeight = 0;
-  for (w of truck_weights) {
-    console.log(tempTrucks);
+  label1: for (w of truck_weights) {
     while (tempWeight + w > weight) {
-      tempTrucks.push(0);
       tempWeight -= tempTrucks[0];
       tempTrucks.shift();
-      answer += 1;
+      if (tempWeight + w > weight) {
+        tempTrucks.push(0);
+        answer += 1;
+        console.log(tempTrucks, answer, "down");
+      } else {
+        tempTrucks.push(w);
+        answer += 1;
+        console.log(tempTrucks, answer, "down");
+        continue label1;
+      }
     }
     if (tempWeight + w <= weight) {
       tempWeight += w;
@@ -20,8 +27,10 @@ function solution(bridge_length, weight, truck_weights) {
       tempWeight -= tempTrucks[0];
       tempTrucks.shift();
       answer += 1;
+      console.log(tempTrucks, answer, "down");
     }
   }
+  answer += bridge_length;
 
   return answer;
 }
