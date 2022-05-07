421ef856fd2bdd5374e36c66a3cb3d769307dc2b
nill <wwaosldk@gmail.com>
2022-05-07 23:45:35 +0900

    truck done!

--- newWeekly/truckOnbridge.js
+++ newWeekly/truckOnbridge.js
@@ -6,29 +6,61 @@ function solution(bridge_length, weight, truck_weights) {
   tempTrucks.length = bridge_length;
   tempTrucks.fill(0);
   let tempWeight = 0;
-  label1: for (w of truck_weights) {
-    while (tempWeight + w > weight) {
+  for (w of truck_weights) {
+    while (tempWeight - tempTrucks[0] + w > weight) {
       tempWeight -= tempTrucks[0];
       tempTrucks.shift();
-      if (tempWeight + w > weight) {
-        tempTrucks.push(0);
-        answer += 1;
-        console.log(tempTrucks, answer, "down");
-      } else {
-        tempTrucks.push(w);
-        answer += 1;
-        console.log(tempTrucks, answer, "down");
-        continue label1;
-      }
-    }
-    if (tempWeight + w <= weight) {
-      tempWeight += w;
-      tempTrucks.push(w);
-      tempWeight -= tempTrucks[0];
-      tempTrucks.shift();
+      tempTrucks.push(0);
       answer += 1;
-      console.log(tempTrucks, answer, "down");
+      console.log(tempTrucks, answer, "up");
     }
+    tempWeight -= tempTrucks[0];
+    tempTrucks.shift();
+    tempWeight += w;
+    tempTrucks.push(w);
+    answer += 1;
+    console.log(tempTrucks, answer, "down");
+
+    // if (tempWeight - tempTrucks[0] + w > weight) {
+    // }
+
+    // tempWeight -= tempTrucks[0];
+    // tempTrucks.shift();
+    // answer += 1;
+    // if (tempWeight + w > weight) {
+    //   tempTrucks.push(0);
+    //   while (tempWeight + w > weight) {
+    //     tempWeight -= tempTrucks[0];
+    //     tempTrucks.shift();
+    //     answer += 1;
+    //     tempTrucks.push(0);
+    //     console.log(tempTrucks, answer, "up");
+    //   }
+    //   tempWeight -= tempTrucks[0];
+    //   tempTrucks.shift();
+    //   answer += 1;
+    //   tempTrucks.push(w);
+    //   console.log(tempTrucks, answer, "up2");
+    //   continue;
+    // }
+
+    // while (tempWeight + w > weight) {
+    //   if (tempWeight + w > weight) {
+    //     tempTrucks.push(0);
+    //     answer += 1;
+    //     console.log(tempTrucks, answer, "down");
+    //   } else {
+    //     tempTrucks.push(w);
+    //     answer += 1;
+    //     console.log(tempTrucks, answer, "down");
+    //     continue label1;
+    //   }
+    // }
+    // if (tempWeight + w <= weight) {
+    //   tempWeight += w;
+    //   tempTrucks.push(w);
+    //   console.log(tempTrucks, answer, "down");
+    // }
   }
   answer += bridge_length;
 
@@ -45,4 +77,4 @@ let truck_weights1 = [7, 4, 5, 6]; //8
 let truck_weights2 = [10]; //101
 let truck_weights3 = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]; //110
 
-console.log(solution(bridge_length1, weight1, truck_weights1));
+console.log(solution(bridge_length2, weight2, truck_weights2));
