8c0dc216215d3a8eec6bed078435906d1838d511
nillworld <wwaosldk@gmail.com>
2022-05-10 14:24:31 +0900

    fineSquare - fail - bigInt

--- newWeekly/fineSquare.js
+++ newWeekly/fineSquare.js
@@ -1,7 +1,7 @@
 // https://programmers.co.kr/learn/courses/30/lessons/62048
 
 function solution(w, h) {
-  let answer = 1;
+  let answer = BigInt(0);
   let long = 0;
   let short = 0;
   if (w >= h) {
@@ -12,10 +12,13 @@ function solution(w, h) {
     short = w;
   }
   let ratio = Math.ceil(long / short);
-  answer = w * h - short * ratio;
-  return answer;
+  answer = BigInt(w) * BigInt(h) - BigInt(short * ratio);
+
+  return answer.toString() * 1;
 }
 
-let w1 = 8;
-let h1 = 12;
+let w1 = 1000000000;
+let h1 = 999999999;
+
+console.log(w1 * h1 - 2 * h1);
 console.log(solution(w1, h1));
