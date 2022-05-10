cf42082381f8edc4fe4ec726337e0ba9ff3d6527
nillworld <wwaosldk@gmail.com>
2022-05-10 16:28:26 +0900

    fineSquare - fail - block of maximum number of pledges

--- newWeekly/fineSquare.js
+++ newWeekly/fineSquare.js
@@ -1,9 +1,12 @@
 // https://programmers.co.kr/learn/courses/30/lessons/62048
 
 function solution(w, h) {
-  let answer = BigInt(0);
+  let answer = 1;
+  let bigIntAnswer = BigInt(0);
   let long = 0;
   let short = 0;
+  let maxM = 1;
+  let partLinedSquare = 0;
   if (w >= h) {
     long = w;
     short = h;
@@ -11,14 +14,33 @@ function solution(w, h) {
     long = h;
     short = w;
   }
-  let ratio = Math.ceil(long / short);
-  answer = BigInt(w) * BigInt(h) - BigInt(short * ratio);
 
-  return answer.toString() * 1;
+  for (let i = short; i >= 2; i--) {
+    if (long % i === 0) {
+      maxM = i;
+      break;
+    }
+  }
+
+  let minLong = long / maxM;
+  let minShort = short / maxM;
+  let ratio = minLong / minShort;
+  let floorRatio = Math.floor(minLong / minShort);
+  if (ratio - floorRatio > 0.5) {
+    partLinedSquare = (floorRatio + 2) * minShort - 2;
+  } else if (ratio - floorRatio === 0) {
+    partLinedSquare = floorRatio * minShort;
+  } else {
+    partLinedSquare = (floorRatio + 1) * minShort;
+  }
+
+  bigIntAnswer = BigInt(w) * BigInt(h) - BigInt(partLinedSquare * maxM);
+  answer = bigIntAnswer.toString() * 1;
+  return answer;
 }
 
-let w1 = 1000000000;
-let h1 = 999999999;
+let w1 = 1;
+let h1 = 1;
 
 console.log(w1 * h1 - 2 * h1);
 console.log(solution(w1, h1));
