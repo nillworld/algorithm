54c996c6efef698408459684419f6b11444587d2
nill <wwaosldk@gmail.com>
2022-05-21 23:30:00 +0900

    openChat

--- newWeekly/openChat.js
+++ newWeekly/openChat.js
@@ -1,6 +1,29 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42888?language=javascript
 
 function solution(record) {
-  var answer = [];
+  let answer = [];
+  let dic = {};
+  for (const a of record) {
+    let splitedRecord = a.split(" ");
+    if (splitedRecord[0] === "Enter") {
+      dic[splitedRecord[1]] = [splitedRecord[2]];
+    }
+    if (splitedRecord[0] === "Change") {
+      dic[splitedRecord[1]] = [splitedRecord[2]];
+    }
+  }
+  for (const a of record) {
+    let splitedRecord = a.split(" ");
+    if (splitedRecord[0] === "Enter") {
+      answer.push(dic[splitedRecord[1]] + "님이 들어왔습니다.");
+    }
+    if (splitedRecord[0] === "Leave") {
+      answer.push(dic[splitedRecord[1]] + "님이 나갔습니다.");
+    }
+  }
+
   return answer;
 }
+
+record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"];
+console.log(solution(record)); //["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]
