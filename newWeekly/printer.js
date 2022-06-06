2b49bd256c33854b9fd604b012b90c7674676f6f
nill <wwaosldk@gmail.com>
2022-06-06 23:44:12 +0900

    set printer

---
+++ newWeekly/printer.js
@@ -0,0 +1,16 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42587
+
+function solution(priorities, location) {
+  let answer = 0;
+
+  for ()
+  return answer;
+}
+
+let priorities1 = [2, 1, 3, 2];
+let priorities2 = [1, 1, 9, 1, 1, 1];
+let location1 = 2;
+let location2 = 0;
+
+console.log(solution(priorities1, location1)); // 1
+console.log(solution(priorities2, location2)); // 5
